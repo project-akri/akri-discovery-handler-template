99006f614d084f2206e8af8093de52a7424fef2b
Kate Goldenring <kate.goldenring@microsoft.com>
2021-02-24 12:36:47 -0800

    use new run_discovery_handler function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 mod discovery_handler;
 
 use akri_discovery_utils::{
-    discovery::{DISCOVERY_HANDLER_PATH, server::run_discovery_server, v0::RegisterRequest},
-    registration_client::{register, register_again},
+    discovery::{discovery_handler::run_discovery_handler},
 };
 use discovery_handler::DiscoveryHandler;
 
@@ -14,35 +13,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>
     // Specify whether the devices discovered by this discovery handler are locally attached (or embedded) to nodes or are
     // network based and usable by multiple nodes.
     let is_local = false;
-    // Specify the socket that the discovery handler will advertize it's service over Instead of using uds, a network
-    // based connection could also be used.
-    let endpoint = format!("{}/{}.sock", DISCOVERY_HANDLER_PATH, name);
     // A Discovery Handler must handle the Agent dropping a connection due to a Configuration that utilizes this 
     // Discovery Handler being deleted or the Agent erroring. It is impossible to determine the cause of the 
     // disconnection, so in case the Agent did error out, the Discovery Handler should try to re-register.
     let (register_sender, register_receiver) = tokio::sync::mpsc::channel(2);
-    // Clone the endpoint for the thread that serves the discovery handler.
-    let endpoint_clone = endpoint.clone();
-    // Run the discovery server.
-    let discovery_handle = tokio::spawn(async move {
-        run_discovery_server(
-            DiscoveryHandler::new(register_sender),
-            &endpoint_clone,
-        )
-        .await
-        .unwrap();
-    });
-    // Create a register request, which specifies the name of the discovery handler, the endpoint for calling it's
-    // discover service, and whether it discovers local or network based devices.
-    let register_request = RegisterRequest {
-        protocol: name.to_string(),
-        endpoint: endpoint.to_string(),
-        is_local,
-    }; 
-    register(&register_request).await?;
-    let registration_handle = tokio::spawn(async move {
-        register_again(register_receiver, &register_request).await;
-    });
-    tokio::try_join!(discovery_handle, registration_handle)?;
+    // Create a discovery handler
+    let discovery_handler = DiscoveryHandler::new(register_sender);
+    run_discovery_handler(discovery_handler, register_receiver, name, is_local).await?;
     Ok(())
 }
