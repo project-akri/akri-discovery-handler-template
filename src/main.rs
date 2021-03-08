c92fb60bb70318a437f78c64ee7f1a20c45f4f05
Kate Goldenring <kate.goldenring@microsoft.com>
2021-03-08 08:38:27 -0800

    change is_local to shared and more inline docs

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,21 @@ use discovery_handler::DiscoveryHandlerImpl;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
-    // Specify the name of this discovery handler. A discovery handler is usually, but not necessarily, identified by
+    // Specify the name of this DiscoveryHandler. A discovery handler is usually, but not necessarily, identified by
     // the protocol it uses.
     let name = "todo";
     // Specify whether the devices discovered by this discovery handler are locally attached (or embedded) to nodes or are
-    // network based and usable by multiple nodes.
-    let is_local = false;
-    // A Discovery Handler must handle the Agent dropping a connection due to a Configuration that utilizes this
-    // Discovery Handler being deleted or the Agent erroring. It is impossible to determine the cause of the
+    // network based and usable/sharable by multiple nodes.
+    let shared = false;
+    // A DiscoveryHandler must handle the Agent dropping a connection due to a Configuration that utilizes this
+    // DiscoveryHandler being deleted or the Agent erroring. It is impossible to determine the cause of the
     // disconnection, so in case the Agent did error out, the Discovery Handler should try to re-register.
     let (register_sender, register_receiver) = tokio::sync::mpsc::channel(2);
-    // Create a discovery handler
+    // Create a DiscoveryHandler
     let discovery_handler = DiscoveryHandlerImpl::new(register_sender);
+    // This function will register the DiscoveryHandler with the Agent's registration socket 
+    // and serve its discover service over UDS at the socket path 
+    // `format!("{}/{}.sock"), env::var("DISCOVERY_HANDLERS_DIRECTORY"), name)`. 
     run_discovery_handler(discovery_handler, register_receiver, name, is_local).await?;
     Ok(())
 }
